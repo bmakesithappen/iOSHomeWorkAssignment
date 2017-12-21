1e0f3b18a9658fcbe51f9a308196c673e85cfd43
Bernard Desert <bdesert@etsy.com>
2017-12-21 16:21:09 -0500

    First push create project

---
+++ iOSHomeworkAssignment/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOSHomeworkAssignment
+//
+//  Created by Bernard Désert on 12/21/17.
+//  Copyright © 2017 Bernard Désert. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
