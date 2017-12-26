cd855e42c148cf5a990f54dec3d5f6443e093e56
Bernard Desert <bdesert@etsy.com>
2017-12-26 14:11:37 -0500

    Added code for tableview functions currently commented out

--- iOSHomeworkAssignment/NowTab.swift
+++ iOSHomeworkAssignment/NowTab.swift
@@ -19,7 +19,25 @@ class NowTab: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+ /*
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
 
 
+    }
+ 
+ */
 }
-
