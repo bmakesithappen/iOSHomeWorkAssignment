0cf5eebf1a4b2e6f6719392bc50f20b9e1d16a4a
Bernard Desert <bdesert@etsy.com>
2017-12-26 11:47:09 -0500

    Add HTTPStatusDataManager file manage data for UIPicker

---
+++ iOSHomeworkAssignment/HTTPStatusDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPStatusDataManager.swift
+//  iOSHomeworkAssignment
+//
+//  Created by Bernard Désert on 12/26/17.
+//  Copyright © 2017 Bernard Désert. All rights reserved.
+//
+
+import UIKit
+
+class HTTPStatusDataManager: NSString {
+
+}
