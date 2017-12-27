89e326f21a1b914ff2376ed4210868a25333fa83
Bernard Desert <bdesert@etsy.com>
2017-12-27 16:34:37 -0500

    Set up tableview in NowTab to show mock data row1 and row2

--- iOSHomeworkAssignment/NowTab.swift
+++ iOSHomeworkAssignment/NowTab.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class NowTab: UIViewController {
-
+class NowTab: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    var rows = ["row1", "row2"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,28 +29,35 @@ class NowTab: UIViewController {
     @IBOutlet weak var tableview: UITableView!
     
     @IBAction func refreshButton(_ sender: Any) {
+        
     }
     
+    // TODO: Review explanation of tableview below.  Breakdown how did we decide "row1 and row2" show.
     
- /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return rows.count
     }
     
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let title = rows[indexPath.row]
+        cell.textLabel?.text = title
         
         return cell
 
 
     }
+     
+     /*
+     
+     {"now": {"epoch": 1514409469.6526053, "slang_date": "today", "slang_time": "now", "iso8601": "2017-12-27T21:17:49.652605Z", "rfc2822": "Wed, 27 Dec 2017 21:17:49 GMT", "rfc3339": "2017-12-27T21:17:49.65Z"}, "urls": ["/", "/docs", "/when/:human-timestamp", "/parse/:machine-timestamp"]}
  
  */
 }
